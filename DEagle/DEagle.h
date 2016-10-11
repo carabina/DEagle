14cb1f41ddf988cd33e1990847ee5eb9ec908cfc
Soroush Khodaii <elrusho@yahoo.co.uk>
2016-10-11 15:18:03 -0700

    Initial Commit

---
+++ DEagle/DEagle.h
@@ -0,0 +1,19 @@
+//
+//  DEagle.h
+//  DEagle
+//
+//  Created by Soroush Khodaii on 2016-10-11.
+//  Copyright © 2016 DataEagle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DEagle.
+FOUNDATION_EXPORT double DEagleVersionNumber;
+
+//! Project version string for DEagle.
+FOUNDATION_EXPORT const unsigned char DEagleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DEagle/PublicHeader.h>
+
+
